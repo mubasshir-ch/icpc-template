367e239472287793c627a7b8a4e0d7ad636aa44e
Mubasshir Chowdhury <md.mubasshir.chowdhury@gmail.com>
2024-10-31 18:21:44 +0600

    Fix tridiagonal overflowing in latex

--- content/numerical/Tridiagonal.h
+++ content/numerical/Tridiagonal.h
@@ -4,18 +4,18 @@
  * License: CC0
  * Source: https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
  * Description: $x=\textrm{tridiagonal}(d,p,q,b)$ solves the equation system
-\[
-\left(\begin{array}{c}b_0\\b_1\\b_2\\b_3\\\vdots\\b_{n-1}\end{array}\right) =
-\left(\begin{array}{cccccc}
+\begin{align*}
+&\left(\begin{array}{c}b_0\\b_1\\b_2\\b_3\\\vdots\\b_{n-1}\end{array}\right) \\
+&= \left(\begin{array}{cccccc}
 d_0 & p_0 & 0 & 0 & \cdots & 0\\
 q_0 & d_1 & p_1 & 0 & \cdots & 0\\
 0 & q_1 & d_2 & p_2 & \cdots & 0\\
 \vdots & \vdots & \ddots & \ddots & \ddots & \vdots\\
 0 & 0 & \cdots & q_{n-3} & d_{n-2} & p_{n-2}\\
 0 & 0 & \cdots & 0 & q_{n-2} & d_{n-1}\\
-\end{array}\right)
-\left(\begin{array}{c}x_0\\x_1\\x_2\\x_3\\\vdots\\x_{n-1}\end{array}\right).
-\]
+\end{array}\right) \\
+& \cdot \left(\begin{array}{c}x_0\\x_1\\x_2\\x_3\\\vdots\\x_{n-1}\end{array}\right).
+\end{align*}
 
 This is useful for solving problems on the type
 \[ a_i=b_ia_{i-1}+c_ia_{i+1}+d_i,\,1\leq i\leq n, \]
